7cc3e9ed089c78ad24da149a012179e9c3839a74
Gil Ferreira <gil.ferreira@mindshift.com>
2016-02-10 11:52:00 -0500

    Added colors to the output

--- echo.js
+++ echo.js
@@ -1,27 +1,39 @@
 // Load our modules
 var fs = require('fs'),
-tabletojson = require('tabletojson'),
-json2csv = require('json2csv'),
-cheerio = require('cheerio');
+  tabletojson = require('tabletojson'),
+  json2csv = require('json2csv'),
+  cheerio = require('cheerio'),
+  colors = require('colors');
 
+// Set defaults
 var defaults = {
   "path" : "process",
   "dest" : "dest",
   "format" : "csv"
 };
 
+// Set theme for output
+colors.setTheme({
+  info: 'green',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red',
+  data: 'grey'
+});
+
+// MARK : Main functions. Part of export
 var convert = (path, dest, format, tableId) => {
 
   //If path is not given kick in defaults
   if(!path){
     path = __dirname+"\\"+defaults.path;
-    console.log("Warning: No path specified. Setting it to ", path);
+    console.log(("Warning: No path specified. Setting it to " + path).warn);
   }
 
   // Do the same for dest
   if(!dest){
     dest = __dirname+"\\"+defaults.dest;
-    console.log("Warning: No destination specified. Setting it to ", dest);
+    console.log(("Warning: No destination specified. Setting it to " + dest).warn);
   }
 
   // Set format type
@@ -31,7 +43,7 @@ var convert = (path, dest, format, tableId) => {
   try {
     // Find
     fs.readdir(__dirname, function(err, files){
-      if(err) { throw new Error(err) };
+      if(err) { throw err };
 
       // Verify path
       var pathIsThere = files.filter(function(obj){
@@ -39,7 +51,9 @@ var convert = (path, dest, format, tableId) => {
       });
 
       if(pathIsThere.length === 0){
-        throw new Error ("Path to process HTML files no there. Path is set to " + path);
+        var err = "Path to process HTML files not there. Path is set to " + path;
+        console.log((err).error);
+        return;
       }
 
       // Verify dest
@@ -49,12 +63,13 @@ var convert = (path, dest, format, tableId) => {
 
       // If not found let us create
       if(destIsThere.length === 0){
-        console.log("Creating destination folder");
+        console.log("Creating destination folder".warn);
 
         try {
           fs.mkdirSync(dest);
         } catch(e) {
-          console.error("Error creating path: ", e.message);
+          var err = "Filesystem error: " + e;
+          console.log((err).error);
         }
       }
 
@@ -68,7 +83,8 @@ var convert = (path, dest, format, tableId) => {
         // Process each HTML
         htmlfiles.forEach(function(file){
 
-          console.log("Working on ", file);
+          var info = "Working on " + file;
+          console.log((info).data);
 
           // Read file content
           try {
@@ -106,45 +122,72 @@ var convert = (path, dest, format, tableId) => {
             p.then(function(info){
 
               var tablesAsJson = tabletojson.convert(info.tables);
-              console.log("Found "+ tablesAsJson.length +" tables");
+              var status = "Found "+ tablesAsJson.length +" tables";
+              console.log((status).data);
 
               //File to write
               tablesAsJson.forEach((table, index) => {
 
                 if(format == 'csv'){
                   json2csv({ data: table}, function(err, csv) {
-                      if(err) { throw new Error(err); }
+                      if(err) throw err;
 
                       var filePath = dest +"/"+info.tableIds[index]+".csv";
+
                       fs.writeFile(filePath, csv, (err) => {
-                        if (err) throw err;
-                        console.log(filePath + ' saved.');
+                        if(err) throw err;
+
+                        var status = "Successfully saved " + filePath;
+                        console.log((status).info);
                       });
                   });
                 } else {
                   filePath = dest +"/"+info.tableIds[index]+".json";
+
                   fs.writeFile(filePath, JSON.stringify(table), (err) => {
                     if (err) throw err;
-                    console.log(filePath + ' saved.');
+
+                    var status = "Successfully saved " + filePath;
+                    console.log((status).info);
                   });
                 }
               });
             })
             .catch(function(e){
-              console.log("Error: ", e);
+              //Catch for promise
+              var err = "Error during converting : " + e;
+              console.log((err).error);
             });
 
           } catch(e) {
-            console.log("Error processing html file : ", e);
+            var err = "Error processing html file : " + e;
+            console.log((err).error);
           }
-
         });
       });
 
     });
   } catch (e) {
-    console.log("Error getting files: ", e);
+    var err = "Error in convert: " + e;
+    console.log((err).error);
   }
 }
 
 exports.convert = convert;
+
+// MARK : Supportive functions
+
+// MARK : Handle errors functions
+function _userException(message){
+  this.message = "User input error: " + message;
+};
+
+function _fsException(message){
+  this.message = message;
+  this.name = "Filesystem error: ";
+};
+
+function _webException(message){
+  this.message = message;
+  this.name = "Web connection error: ";
+};
